d9a1a735af6e3dffc4f8a8a53a9a1e137144bfeb
DARK SHADOW <s4b7zb@gmail.com>
2025-04-26 13:05:41 +0600

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,8 +3,7 @@ pkg install -y root-repo
 pkg install -y git tsu python wpa-supplicant pixiewps iw
 
 git clone https://github.com/dark-s4b7z/SHADOW-ONESHOT.git
-ls
-cd SHADOW-ONESHOT
-chmod +x oneshot.py
+
+chmod +x SHADOW-ONESHOT/oneshot.py
 
 printf "###############################################\n#  All done! Now you can run OneShot with\n#   sudo python neshot.py -i wlan0 -K\n#\n#  MADE BY DARK SHADOW,\n#\n###############################################\n"
