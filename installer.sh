021f8ba6e96aff2dc91da07ac7ae34a89c801528
DARK SHADOW <s4b7zb@gmail.com>
2025-04-26 13:02:03 +0600

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,6 +3,7 @@ pkg install -y root-repo
 pkg install -y git tsu python wpa-supplicant pixiewps iw
 
 git clone https://github.com/dark-s4b7z/SHADOW-ONESHOT.git
+ls
 cd SHADOW-ONESHOT
 chmod +x oneshot.py
 
